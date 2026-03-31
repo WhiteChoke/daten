4671359971dea9ad1fb78615c6851964bf38e241
WhiteChoke <denisduchenkoo@gmail.com>
2026-03-31 15:26:41 +0900

    in process: profile service, validate profile

--- userService/src/main/java/dev/whitechoke/userService/domain/ProfileService.java
+++ userService/src/main/java/dev/whitechoke/userService/domain/ProfileService.java
@@ -1,4 +1,20 @@
-package dev.whitechoke.userService.domain.db;
+package dev.whitechoke.userService.domain;
 
+import dev.whitechoke.userService.api.dto.ProfileCreateRequestDto;
+import dev.whitechoke.userService.api.dto.ProfileCreateResponseDto;
+import dev.whitechoke.userService.domain.db.ProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ProfileService {
+
+//    private final ProfileRepository profileRepository;
+//
+//    public ProfileCreateResponseDto createProfile(
+//            ProfileCreateRequestDto request
+//    ) {
+//
+//    }
 }
--- userService/src/main/java/dev/whitechoke/userService/utils/ValidateProfile.java
+++ userService/src/main/java/dev/whitechoke/userService/utils/ValidateProfile.java
@@ -1,4 +1,28 @@
 package dev.whitechoke.userService.utils;
 
+import dev.whitechoke.userService.api.dto.ProfileDto;
+
 public class ValidateProfile {
+
+    public <T extends ProfileDto> void validate(T dto) {
+        if (dto.birthday() == null) {
+            throw new IllegalArgumentException("Birthday cant be null");
+        }
+        if (dto.telegramId() == null) {
+            throw new IllegalArgumentException("Telegram id cant be null");
+        }
+        if (dto.gender() == null) {
+            throw new IllegalArgumentException("Gender cant be null");
+        }
+        if (dto.longitude() == null) {
+            throw new IllegalArgumentException("Longitude cant be null");
+        }
+        if (dto.latitude() == null) {
+            throw new IllegalArgumentException("Latitude cant be null");
+        }
+        if (dto.name() == null || dto.name().isBlank()) {
+            throw new IllegalArgumentException("Gender cant be empty");
+        }
+
+    }
 }
