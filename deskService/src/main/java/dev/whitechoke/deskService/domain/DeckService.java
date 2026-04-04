cde0a8042dca82872a4dfff2641ad7e3077c3162
WhiteChoke <denisduchenkoo@gmail.com>
2026-04-04 14:37:30 +0900

    created deck service

--- deskService/src/main/java/dev/whitechoke/deskService/domain/DeckService.java
+++ deskService/src/main/java/dev/whitechoke/deskService/domain/DeckService.java
@@ -1,4 +1,31 @@
 package dev.whitechoke.deskService.domain;
 
+import dev.whitechoke.commonLibs.http.FormResponseDto;
+import dev.whitechoke.deskService.http.ProfileHttpClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class DeckService {
+
+    private final ProfileHttpClient profileHttpClient;
+    private final DeckCreator deckCreator;
+    private final RedisTemplate<String, Long> redisTemplate;
+    @Value("${redis-deck-prefix}")
+    private String deckPrefix;
+
+    private final FormResponseDto getForm(Long telegramId) {
+
+        var key = deckPrefix + telegramId;
+
+        if (redisTemplate.opsForSet().size(key) < 10) {
+            deckCreator.updateDeck(telegramId);
+        }
+
+        Long profileId = redisTemplate.opsForSet().pop(key);
+        return profileHttpClient.getForm(profileId);
+    }
 }
