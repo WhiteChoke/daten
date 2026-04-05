c4f3a789009d1e0fd8dd8a349c82de5afbca1781
WhiteChoke <denisduchenkoo@gmail.com>
2026-04-05 14:53:07 +0900

    created deck http client

--- matchService/src/main/java/dev/whitechoke/matchService/http/DeckHttpClient.java
+++ matchService/src/main/java/dev/whitechoke/matchService/http/DeckHttpClient.java
@@ -1,4 +1,16 @@
 package dev.whitechoke.matchService.http;
 
+import dev.whitechoke.commonLibs.http.FormResponseDto;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+@HttpExchange(
+        accept = "application/json",
+        contentType = "application/json",
+        url = "/api/v1/decks"
+)
 public interface DeckHttpClient {
+    @GetExchange(url = "{telegramId}")
+    FormResponseDto getForm(@PathVariable Long telegramId);
 }
--- matchService/src/main/java/dev/whitechoke/matchService/http/DeckHttpClientConfig.java
+++ matchService/src/main/java/dev/whitechoke/matchService/http/DeckHttpClientConfig.java
@@ -1,4 +1,30 @@
 package dev.whitechoke.matchService.http;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
 public class DeckHttpClientConfig {
+
+    @Value("${http-url.deck-service}")
+    private String deckServiceUrl;
+
+    @Bean
+    public RestClient deckRestClient() {
+        return RestClient.builder()
+                .baseUrl(deckServiceUrl)
+                .build();
+    }
+
+    @Bean
+    public DeckHttpClient deckHttpClient() {
+        return HttpServiceProxyFactory.builder()
+                .exchangeAdapter(RestClientAdapter.create(deckRestClient()))
+                .build()
+                .createClient(DeckHttpClient.class);
+    }
 }
