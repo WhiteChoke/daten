b22a6fb85a9df4e56854d5dc93579b858f0cbc5c
WhiteChoke <denisduchenkoo@gmail.com>
2026-04-06 16:39:14 +0900

    created match service and controller

--- matchService/src/main/java/dev/whitechoke/matchService/domain/MatchService.java
+++ matchService/src/main/java/dev/whitechoke/matchService/domain/MatchService.java
@@ -1,10 +1,35 @@
-package dev.whitechoke.matchService;
+package dev.whitechoke.matchService.domain;
+
+import dev.whitechoke.commonLibs.http.FormResponseDto;
+import dev.whitechoke.matchService.domain.db.MatchEntity;
+import dev.whitechoke.matchService.domain.db.MatchRepository;
+import dev.whitechoke.matchService.http.DeckHttpClient;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MatchService {
 
+    private final DeckHttpClient deckHttpClient;
+    private final MatchRepository matchRepository;
+
+
+    public FormResponseDto getMatch(Long telegramId) {
+
+        var form = deckHttpClient.getForm(telegramId);
+
+        saveMatch(telegramId, form.telegramId());
+
+        return form;
+    }
+
+    @Transactional
+    private void saveMatch(Long firstTgId, Long secondTgId) {
+        var match = new MatchEntity();
+        match.setIds(firstTgId, secondTgId);
+        matchRepository.save(match);
+    }
 }
--- matchService/src/main/java/dev/whitechoke/matchService/domain/api/MatchController.java
+++ matchService/src/main/java/dev/whitechoke/matchService/domain/api/MatchController.java
@@ -1,4 +1,30 @@
 package dev.whitechoke.matchService.domain.api;
 
+import dev.whitechoke.commonLibs.http.FormResponseDto;
+import dev.whitechoke.matchService.domain.MatchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/matchs")
 public class MatchController {
+
+    private final MatchService service;
+
+    @GetMapping("{telegramId}")
+    public ResponseEntity<FormResponseDto> getMatch(
+            @PathVariable Long telegramId
+    ) {
+        var response = service.getMatch(telegramId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+    }
 }
